efa6d8adeb4dc9ab46235bb74fd6ad7bdc8d82d9
Kashyapbd7 <kashyapbd7@gmail.com>
2026-04-08 12:46:08 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -63,4 +63,4 @@ app.get("/api/markets", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Running on port " + PORT));
+app.listen(PORT, "0.0.0.0", () => console.log("Running on port " + PORT));
